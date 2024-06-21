a495d2f212963bc79df82311f34a840618b20610
lekhactung <tungle07102005@gmail.com>
2024-06-21 20:05:49 +0700

    upd

--- LAB/LAB1_LIST/2.cpp
+++ LAB/LAB1_LIST/2.cpp
@@ -24,24 +24,25 @@ void output(int a[],int n){
 void add_index(int a[],int &n,int vt,int x){
     cout << "Nhap vi tri phan tu can them gia tri vua nhap: ";
     cin >> vt;
-    if(vt>0 && vt <=MAX){
+    if(vt>0 && vt <=n+1){
         for(int i=n;i>=vt;i--){
             a[i]=a[i-1];
         }
         a[vt-1]=x;
         n++;
+        cout <<"Da them! \n";
     } else {
-        cout <<"Vi tri khong hop le!";
+        cout <<"Vi tri khong hop le!\n";
     }
 }
 
-bool sequential_search(int a[],int n,int x){
+int sequential_search(int a[],int n,int x){
     for(int i=0;i<n;i++){
         if(a[i]==x){
-            return true;
+            return i;
         }
     }
-    return false;
+    return -1;
 }
 
 void swap(int &a,int &b){
@@ -77,14 +78,110 @@ int binary_search(int a[],int n,int x){
     return -1;
 }
 
+void del_arr(int a[],int &n,int x){
+    if(sequential_search(a,n,x)!=-1){
+        for(int i=sequential_search(a,n,x);i<n;i++){
+            a[i] = a[i+1];
+        }
+        n--;
+        cout << "Da xoa!";
+    } else {
+        cout << "Gia tri khong ton tai trong mang";
+    }
+}
+
+int *gopmang(int a[],int b[],int n,int m){
+    int *c = new int [n+m];
+    int index =0; 
+    for(int i=0;i<n;i++){
+        c[index++] = a[i];
+    }
+    for(int i=0;i<m;i++){
+        c[index++] = b[i];
+    }
+    sort(c,n+m);
+    return c;
+}
+
+void menu(){
+    cout << "1. Them mot phan tu vao danh sach \n"
+        <<"2. Xuat cac phan tu trong danh sach \n"
+        <<"3. Tim mot phan tu trong danh sach bang tim kiem tuan tu \n"
+        <<"4. Tim mot phan tu trong danh sach bang tim kiem nhi phan \n"
+        <<"5. Tim mot phan tu trong danh sach, xoa phan tu neu tim duoc \n"
+        <<"6. Nhap mang thu 2  \n"
+        <<"7. Gop 2 danh sach dac da co thu tu thanh 1 danh sach co thu tu \n";
+}
+
 int main(){
-    int a[MAX],n,vt,x ;
-
+    int a[MAX],n,vt,x;
+    int b[MAX],m;
+    int choice;
+    bool flag = false;
     input(a,n);
-    sort(a,n);
-    output(a,n);
-    cout << endl;
-    cout << a[binary_search(a,n,4)];
+    int *c;
+    do{
+        
+        do{
+            system("cls");
+            menu();
+            cout << "Nhap chuong trinh : ";
+            cin >> choice;
+            if(choice !=0 && choice !=1 &&choice !=2 &&choice !=3 &&choice !=4 &&choice !=5 &&choice !=6 && choice !=7) {
+                cout <<"Vui long nhap lai!" << endl;
+            }
+        } while( choice!=0 && choice !=1 &&choice !=2 &&choice !=3 &&choice !=4 &&choice !=5 &&choice !=6 && choice !=7);
 
+        switch (choice)
+        {
+        case 1:
+            cout <<"Nhap gia tri can them: ";
+            cin >> x;
+            add_index(a,n,vt,x);
+            break;
+        
+        case 2:
+            cout << "Cac gia tri cua mang thu 1  : \n ";
+            output(a,n);
+            break;
+        
+        case 3:
+            cout << "Nhap gia tri can tim: ";cin >> x;
+            if(sequential_search(a,n,x)!=-1){
+                cout << "Phan tu co trong danh sach! \n";
+            } else{
+                cout <<"Phan tu khong co trong danh sach! \n";
+            }
+            break;
+        case 4: 
+            cout << "Nhap gia tri can tim : "; cin >> x;
+            if(binary_search(a,n,x)!=-1){
+                cout << "Phan tu co trong danh sach! \n";
+            } else{
+                cout <<"Phan tu khong co trong danh sach! \n";
+            }
+            break;
+        case 5:
+            cout <<"Nhap phan tu can tim va xoa: "; cin >> x;
+            del_arr(a,n,x);
+            break;
+        case 6:
+            input(b,m);
+            flag = true;
+            break;
+        case 7:
+            if(flag){
+                c = new int(n+m);
+                c = gopmang(a,b,n,m);
+                output(c,n+m);
+            } else{
+                cout <<"Chua khai bao mang thu 2! \n";
+            }
+            break;
+        default:
+            break;
+        }
+        system("pause");
+    } while(choice !=0 );
     return 0;
 }
--- LAB/LAB1_LIST/test.cpp
+++ LAB/LAB1_LIST/test.cpp
@@ -1,171 +1,58 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
-
-struct Node{
-    int info;
-    Node *link;
-};
-
-void init(Node *&head){
-    head = NULL;
-}
-
-Node* createNode (int x){
-    Node *p = new Node;
-    p->info = x;
-    p->link = NULL;
-    return p;
-}
-
-void insertLast(Node *&head,int x){
-    Node *p = createNode(x);
-    if(head == NULL){
-        head = p;
-    } else {
-        Node *plast = head;
-        while(plast->link != NULL){
-            plast = plast->link;
+void input(int a[],int &n){
+        for(int i=0;i<n;i++){
+            cin >> a[i];
         }
-        plast->link = p;
-    }
-    
 }
 
-void insertFirst(Node *&head,int x){
-    Node *p = createNode(x);
-    if(head == NULL){
-        head = p;
-    } else {
-        p->link = head;
-        head = p;
-    }
-    
-}
-
-void delFirst(Node *&head){
-    if(head!=NULL){
-        Node *p = head;
-        head = head->link;
-        p->link = NULL;
-        delete p;
+void output(int a[],int n){
+    for(int i=0;i<n;i++){
+        cout << a[i] << " ";
     }
 }
 
-void delLast(Node *&head){
-    Node *p = head;
-    if(head!=NULL){
-        Node *plast = head;
-        Node *previousLast = NULL;
-        while(plast->link != NULL){
-            previousLast = plast;
-            plast = plast->link;
+void swap(int &a,int &b){
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+void sort(int a[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(a[i]<=a[j]){
+                swap(a[i],a[j]);
+            }
         }
-        previousLast->link = NULL;
-        delete previousLast;
-        plast = NULL;
     }
 }
 
-
-void output(Node *head){
-    Node *p = head;
-    while(p!=NULL){
-        cout << p->info << " ";
-        p = p->link;
+int *gopmang(int a[],int b[],int n,int m){
+    int *c = new int [n+m];
+    int index =0; 
+    for(int i=0;i<n;i++){
+        c[index++] = a[i];
     }
-    cout << endl;
-}
-
-// Node* search(Node *head,int x){
-//     Node *p = head;
-//     while(p!=NULL && p->info != x){
-//         p = p->link;
-//     }
-//     return p;
-// }
-
-bool search( Node *head, int x){
-    Node *p = head;
-    while(p->link!=NULL){
-        if(p->info == x){
-            return true;
-        }
-        p = p->link;
+    for(int i=0;i<m;i++){
+        c[index++] = b[i];
     }
-    return false;
-}
-
-int count(Node *head){
-    int cnt=0;
-    while(head!=NULL){
-        ++cnt;
-        head=head->link;
-    }
-    return cnt;
-}
-
-void deleteNode(Node *&head,int x){
-    if(head==NULL){
-        return;
-    } 
-    if(head->info == x){
-        Node  *tmp = head;
-        head = head->link;
-        delete tmp;
-        return;
-    }
-    Node *p = head;
-    Node *prev = NULL;
-    while(p!=NULL && p->info!=x){
-        prev = p;
-        p = p->link;
-    }
-    if(p==NULL){
-        return;
-    }
-    prev->link = p->link;
-    delete prev;    
-    return;
-}
-
-void menu(){
-    cout << "1. Xuat cac phan tu trong danh sach \n"
-        <<"2. Tim phan tu trong danh sach \n"
-        <<"3.Them phan tu vao dau danh sach \n"
-        <<"4.Xoa phan tu dau danh sach \n"
-        << "5.Them phan tu vao cuoi danh sach \n"
-        <<"6.Xoa phan tu cuoi danh sach \n"
-        <<"7.Tim phan tu trong danh sach va xoa phan tu do neu tim duoc \n";
+    sort(c,n+m);
+    return c;
 }
 
 int main(){
-
-    Node *head;
-    init(head);
-    int choice,x;
-    insertFirst(head,10);
-    insertFirst(head,20);
-    insertFirst(head,30);
-    insertFirst(head,40);
-    insertFirst(head,50);
-    insertLast(head,5);
-    insertLast(head,99);
-    insertLast(head,87);
-    insertLast(head,100);
-    insertLast(head,123);
-    output(head);
-    // cout << count(head);
-    deleteNode(head,50);
-
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int n =5 , m =5;
+    int a[n],b[m];
+    int *c = new int [n+m];
+    input(a,n);
+    input(b,m);
+    c = gopmang(a,b,n,m);
+    output(a,n);
+    output(b,m);
     cout << endl;
-    output(head);
-    deleteNode(head,10);
-
-    cout << endl;
-    output(head);
-    deleteNode(head,123);
-
-    cout << endl;
-    output(head);
+    output(c,n+m);
     return 0;
 }
