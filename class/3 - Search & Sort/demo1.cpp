343fddecb2204562684fb969c5f17be99fbc8409
lekhactung <tungle07102005@gmail.com>
2024-07-15 15:08:27 +0700

    upd

--- class/3 - Search & Sort/demo1.cpp
+++ class/3 - Search & Sort/demo1.cpp
@@ -29,9 +29,41 @@ void quickSort(int a[],int left,int right){
     if(l<right){
         quickSort(a,l,right);
     }
- }
+}
 
- void output(int a[],int n){
+void heapify (int a[],int n,int i){
+    int largest = i;
+    int l = 2*i+1;
+    int r = 2*i+2;
+    if(l<n && a[l] > a[largest]){
+        largest = l;
+    }
+    if(r<n && a[r] && a[r] > a[largest]){
+        largest = r;
+    }
+    if(largest != i){
+        swap(a[largest],a[i]);
+        heapify(a,n,largest);
+    }
+}
+
+void heapsort(int a[],int n){
+    int i = n/2-1;
+    while(i>=0){
+        heapify(a,n,i);
+        i--;
+    }
+    int right = n-1;
+    while(right>0){
+        swap(a[0],a[right]);
+        if(right>0){
+            heapify(a,right,0);
+        }
+        right--;
+    }
+}
+
+void output(int a[],int n){
     int i=0;
     while(i<n){
         cout << a[i++] << " ";
@@ -41,7 +73,8 @@ void quickSort(int a[],int left,int right){
 int main(){
     int a[]={2,5,1,3,6,10,7,1,15,100,20,99,1};
     int n = sizeof(a)/sizeof(a[0]);
-    quickSort(a,0,n-1);
+    // quickSort(a,0,n-1);
+    heapsort(a,n);
     output(a,n);
     return 1;
 }
--- class/3 - Search & Sort/test.cpp
+++ class/3 - Search & Sort/test.cpp
@@ -147,6 +147,38 @@ int *mergeSort (int a[],int n){
     return merge(m1,mid,m2,n-mid);
 }
 
+void heapify(int a[],int n,int i){
+    int largest = i;
+    int l = 2*i+1;
+    int r = 2*i+2;
+    if(l<n && a[l]>a[largest]){
+        largest = l;
+    }
+    if(r < n && a[r]>a[largest]){
+        largest = r;
+    }
+    if(largest!=i){
+        swap(a[i],a[largest]);
+        heapify(a,n,largest);
+    }
+}
+
+void heapsort(int a[],int n){
+    int i = n/2+1;
+    while(i>=0){
+        heapify(a,n,i);
+        i--;
+    }
+    int right = n-1;
+    while(right>0){
+        swap(a[0],a[right]);
+        if(right>0){
+            heapify(a,right,0);
+        }
+        right--;
+    }
+}
+
 void output(int a[],int n){
     int i=0;
     while(i<n){
@@ -165,15 +197,17 @@ int main(){
     //     cout << d[i] << " ";
     // }
     // delete []d;
+
     output(a,n);
     // // selectionSort(a,n);
     // // bubbleSort(a,n);
     // // insertionSort(a,n);
     // interchangeSort(a,n);
     // quickSort(a,0,n-1);
-    
-    int *b = mergeSort(a,n);
-    output(b,n);
-    delete []b;
+    heapsort(a,n);
+    output(a,n);
+    // int *b = mergeSort(a,n);
+    // output(b,n);
+    // delete []b;
     return 1;
 }
